89fc5b9100f7636ad8e68763b53bb20a7ca556cd
Your Name <you@example.com>
2021-07-21 16:11:39 +0800

    FEAT 功能开发完成

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# sentry_webhook_wxpusher
-Sentry webhook push wechat from wxpusher
+# sentry网络钩子 创建gitee issue
+
+项目中使用了sentry，可以配合这个项目给gitee仓库创建issue。可以把问题和解决关联起来
+
+*commit时加入`CLOSE #ISSUEID`可关闭issue并关联起来*
+
+
+## 截图
+
+sentry
+![](./test1.png)
+
+gitee
+![](./test.png)
+
+
+
+## 安装使用
+
+#### 1.克隆仓库并部署（后台部署可以使用`pm2`）
+```
+git clone https://github.com/edk24/sentry_webhook_wxpusher.git
+```
+
+#### 2.配置
+
+```
+cp config.json.example config.json 
+
+按照要求配置文件
+```
+
+#### 3.运行
+
+```
+npm install
+npm start
+```
+
+访问地址 http://xxx.xxx.xxx.xxx:8081 出现`hello`表示部署成功
+
+#### 4.配置sentry webhook 
+
+打开`sentry`后台，找到`Setting -> Developer settings -> INTERNAL INTEGRATIONS` 创建
+
+`Alert Rule Action`启用，
+
+`Webhook URL`填入‘http://你的地址/sentry’
+
+`issue`和`error`选中，最后保存触发一个错误试试吧
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let axios = require('axios');
 let app = express();
 
-// index
+const config = require('./config.json');
+
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
-   res.send('hello');
+  res.send('hello');
 });
 
-// webhook
-app.post('/sentry', function(req, res) {
-    console.log(req.body);
+app.post('/sentry', function (req, res) {
+  if (req.body.action == 'created' && typeof req.body.data.error == 'object') {
+    let body = [];
+
+    body.push(`- 运行环境：${req.body.data.error.environment}`);
+    body.push(`- 罪魁祸首：${req.body.data.error.culprit}`)
+    body.push(`- 文件位置：\`${req.body.data.error.location}\``);
+    body.push(`- 问题定位：(${req.body.data.error.web_url})`);
+
+    axios.post(`https://gitee.com/api/v5/repos/${config.gitee.owner}/issues`, {
+      "access_token": config.gitee.access_token,
+      "repo": config.gitee.repo,
+      "title": req.body.data.error.title,
+      "labels": 'bug',
+      "body": body.join("\n")
+    });
+  }
+  res.end('ok');
 });
- 
-const server = app.listen(8081, function () {
+
+
+const server = app.listen(config.server.port, function () {
   const host = server.address().address
   const port = server.address().port
- 
+
   console.log("host http://%s:%s", host, port)
 })
